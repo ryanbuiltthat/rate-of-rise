e6357fc6c2c3c338e19734588f6e0665be4e5af6
Ryan Harris <ryanbuiltthat@users.noreply.github.com>
2026-09-15 21:39:15 -0400

    Update main.cpp

--- firmware/moteino_creek_node/src/main.cpp
+++ firmware/moteino_creek_node/src/main.cpp
@@ -42,7 +42,7 @@
 #define ENCRYPT_KEY   "KksDNqcNb6mCY4xA"
 //This setting enables this gateway to work with remote nodes that have ATC enabled to
 //dial their power down to only the required level (ATC_RSSI)
-#define ENABLE_ATC    //comment out this line to disable AUTO TRANSMISSION CONTROLg
+//#define ENABLE_ATC    //comment out this line to disable AUTO TRANSMISSION CONTROLg
 #define ATC_RSSI      -85
 
 // ─── Pins ────────────────────────────────────────────────────────────────────
